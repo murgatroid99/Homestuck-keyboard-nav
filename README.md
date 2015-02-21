5e49d95cce0e208fe8134c5c8bb18025c5f853d8
Michael Lumish <michael.lumish@gmail.com>
2015-02-20 19:42:09 -0800

    Added support for non-Homestuck comics

---
+++ README.md
@@ -0,0 +1,15 @@
+#Homestuck Keyboard Navigation
+
+## Installation
+
+This chrome extension can be installed [here](https://chrome.google.com/webstore/detail/homestuck-keyboard-naviga/icfppdnbakiampejgikedpidpnpcniga).
+
+## Usage
+
+ - Right Arrow key: go to the next page
+ - Left Arrow key: go to the previous page
+ - L or Ctrl key: open or close a dialoglog
+
+## Notes
+
+Arrow key navigation is disabled on flash pages because it interferes with playing the flash games.
--- js/content_script.js
+++ js/content_script.js
@@ -1,22 +1,29 @@
 (function(){
   "use strict";
-  
-  var pageNum = parseInt(/p=(\d+)/.exec(document.location.search)[1]);
-  var prev = "/index.php?s=6&p=00" + (pageNum - 1);
-  var next = "/index.php?s=6&p=00" + (pageNum + 1);
-  var nextPage;
-  function prevPage(){
-    document.location = prev;
-  };
-  var link = document.querySelector('font[size="5"] a');
-  if(link){
-    nextPage = function(){
-      link.click();
+
+  var pageNum, prev, next, nextPage, prevPage, comicNum;
+  if(!document.querySelector("embed")){
+    pageNum = parseInt(/p=(\d+)/.exec(document.location.search)[1]);
+    comicNum = parseInt(/s=(\d+)/.exec(document.location.search)[1]);
+    prev = path + "?s=" + comicNum + "&p=" + pad6(pageNum - 1);
+    next = path + "?s=" + comicNum + "&p=" + pad6(pageNum + 1);
+    nextPage;
+    prevPage = function(){
+      document.location = prev;
     };
-  } else {
-    nextPage = function(){
-      document.location = next;
+    var link = document.querySelector('font[size="5"] a');
+    if(link){
+      nextPage = function(){
+        link.click();
+      };
+    } else {
+      nextPage = function(){
+        document.location = next;
+      }
     }
+  } else {
+    prevPage = function(){};
+    nextPage = function(){};
   }
   
   var buttons = document.getElementsByClassName('button');
@@ -32,6 +39,7 @@
 
   document.body.onkeydown = function(event){
     switch(event.keyCode){
+    case 17:
     case 76: toggleSpoiler(); break;
     case 39: nextPage(); break;
     case 37: prevPage(); break;
