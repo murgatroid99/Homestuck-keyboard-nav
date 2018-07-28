53ea94033d3a26400daf8a70bcdf3bfb7abfa37a
wkalb <will.kalb@gmail.com>
2018-07-28 10:30:03 -0400

    adds more rubust functionality for CYA pages and retcon pages

--- js/content_script.js
+++ js/content_script.js
@@ -8,28 +8,78 @@
     pageNum = 1;
   }
   prev = path + Math.max(1, pageNum - 1);
+  next = path + (pageNum + 1);
+
 
   var navContainer = document.querySelector(".o_story-nav");
   var navLinks = navContainer.querySelectorAll("a");
-  var nextPageLink = navLinks[navLinks.length - 1]; // Deals with pages with a "[???????]" link before the next page link
+  var navButton = navContainer.querySelector("button"); // Pages with an "3NT3R P4SSWORD" button instead of a link
 
-  function nextPage(usedArrowKey){
-    if (!(usedArrowKey && document.querySelector("embed"))){ //disables arrow key navigation on flash pages
-	if (nextPageLink){
-	    nextPageLink.click();
-	}
-    }
-  };
-  function prevPage(usedArrowKey){
-    if (!(usedArrowKey && document.querySelector("embed"))){ //disables arrow key navigation on flash pages
+  var nextPageElement; 
+  if (navLinks) {
+    nextPageElement = navLinks[navLinks.length - 1]; // Ignores [??????] links on retconned pages
+  }
+  else if (navButton) {
+    nextPageElement = navButton; // Relevant on pages with a password input form instead of a link
+  }
+  else {
+    nextPageElement = null;
+  }
+
+  // Forward navigation
+
+  // Disables arrow key navigation on flash game pages, but allows "." navigation
+  if (document.querySelector("embed")){   
+    nextPage = function(usedArrowKey) {
+      if (!usedArrowKey) {
+        document.location = next;
+      }
+    };
+  }
+  // Moves to the next sequential page on Select Your Character pages instead of clicking the "skip to end" link
+  else if (document.querySelector("canvas")) { 
+    nextPage = function(usedArrowKey) {
+      document.location = next;
+    };
+  }
+  // Pages with no "next" link and no "3NT3R P4SSWORD" button to click, usually with the "next" link embeded somewhere else
+  // This case should be synonymous with with the "flash game pages" case, but it is included for unforeseen exceptions
+  else if (!nextPageElement){ //
+    nextPage = function(usedArrowKey) {
+      document.location = next; 
+    };
+  }
+  // All other cases, typically a standard page or an "3NT3R P4SSWORD" page.
+  // On a standard page, the "next" link is clicked. On an "3NT3R P4SSWORD" page, the "Submit" button is clicked
+  else {
+    nextPage = function(usedArrowKey) {
+      nextPageElement.click();
+    };
+  }
+
+  // Backward navigation
+
+  // Disables arrow key navigation on flash game pages, but allows "," navigation
+  if (document.querySelector("embed")){   
+    prevPage = function(usedArrowKey) {
+      if (!usedArrowKey) {
+        document.location = prev;
+      }
+    };
+  }
+  // All other cases
+  else {
+    prevPage = function(usedArrowKey) {
       document.location = prev;
-    }
-  };
- 
-  
+    };
+  }
+
+  // Opens chat logs  
   chatButtons = document.getElementsByClassName('o_chat-log-btn');
   function toggleLog(){
+    if (chatButtons) {
       chatButtons[0].click();
+    }
   };
 
   document.body.onkeydown = function(event){
